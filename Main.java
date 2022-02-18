1cabfa452d768d9cfac26f2ac262799fc4c91e6d
KvasarINC <99033916+KvasarINC@users.noreply.github.com>
2022-02-18 20:13:48 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.learning;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Robots robot1 = new Robots();
+
+        robot1.name = "Bogdan";
+        robot1.old = 17;
+        robot1.width = 69.6f;
+        robot1.hight = 174.7f;
+
+        robot1.ResultSports();
+    }
+
+}
